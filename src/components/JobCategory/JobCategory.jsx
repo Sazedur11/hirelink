6a6ab6ea17448d9ecb58a88efeb7978c70b5f950
Sazedur <sazedur11ab@gmail.com>
2025-08-01 23:49:27 +0600

    job category updated

--- src/components/JobCategory/JobCategory.jsx
+++ src/components/JobCategory/JobCategory.jsx
@@ -4,10 +4,10 @@ const JobCategory = ({job}) => {
     console.log(job);
     const {logo, category_name, availability} = job;
     return (
-        <div>
+        <div className="bg-base-200 p-6 rounded-md">
             <img src={logo} alt={category_name} 
             className="w-12 p-2 bg-base-300 rounded-sm mb-4"/>
-            <h2 className="font-bold">{category_name}</h2>
+            <h2 className="font-bold my-1">{category_name}</h2>
             <p className="text-sm text-gray-500">{availability}</p>
         </div>
     );
